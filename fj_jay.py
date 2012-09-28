bd9d4ac05e67593cc783cc16af1bff2a35c74f23
Jay <jlaura@asu.edu>
2012-09-28 10:48:37 -0700

    WIP ErrorMat

--- fj_jay.py
+++ fj_jay.py
@@ -6,6 +6,80 @@ import time
 import multiprocessing
 import sharedmem_sample
 
+def partitionErr(cores, numErr):
+    '''
+    A binary search in range[lo,hi] where x = k.
+
+    lo is the maximum value of an element in the input array
+    hi is the cumsum of the array
+    k = the number of workers available (cores)
+    x = a variable number of workers used to calculate the ideal division of work
+    
+    Example
+    -------
+    A_values = [120,108, 110, 108, 108, 108, 106, 108, 103, 103, 103, 104, 105, 102, 100, 99]
+    
+    A_calcs = array([  0,   1,   2,   3,  4,  5,  6,  7,  8,  9,  10,  11,  12, 13, 14, 15])
+    A_calcs is the number of combinations to test per index.
+    
+    hi_cumsum = array([  0,   1,   3,   6,  10,  15,  21,  28,  36,  45,  55,  66,  78, 91, 105, 120])
+    Where hi_cumsum is the cumsum of A_cumsum.  That is, if k were to do all the calculations in A_cumsum
+    k = 4
+    
+    hi = 120
+    lo = 15
+    
+    The goal is to find the maximum values in the range [lo, hi], where x = k
+    
+    At the lowest cost x = 10:
+    [0, 1, 3, 4, 5],  [6, 7], [8], [9], [10], [11], [12], [13], [14], [15] 
+    
+    We then divide the range in half, picking the lower half and divide again since x > k.
+    
+    At 15 x = 10
+    At 67.5 x = 2
+    At 120 x = 1
+    
+    Since x=2 < k, take the bottom half of the search range and iterate. [15, 67.5]
+    
+    At 15 x = 10
+    At 41.25 x = 4
+    At 67.5 x = 2
+    Since x = 4 = k, we are done in a total of 2 iterations.
+    
+    '''
+    
+    lo = numpy.amax(numpy.arange(numErr))
+    values = numpy.cumsum(numpy.arange(numErr))
+    hi = values[-1]
+    k = cores
+    
+    print lo, hi
+    
+    
+    while lo < hi:
+	mid = lo + (hi-lo)/2
+	x = getX(values,mid)
+	print mid, x
+	if x <= k:
+	    hi = mid
+	else:
+	    lo = mid + 1
+    return lo
+
+def getX(values,mid):
+    total = 0
+    index = 0
+    numK = 1
+    for x in values:
+	total += x
+	print total
+	if total > mid:
+	    total = values[index]
+	    numK += 1
+	index += 1
+    return numK   
+
 def allocate(values, classes=5, sort=False):
     numClass = classes
     if sort:
@@ -108,36 +182,45 @@ def main():
     
     #Calculate the error matrix
     sharedErr.asarray()[0] = sharedVar.asarray()[0]
-
-    for j in xrange(1,numClass):
-	step= len(values)/cores #Naive split of the errorMat row into equal segments
-	for k in range(0,len(values),step):
-	    p = multiprocessing.Process(target=errPop, args=(sharedErr.asarray()[j], slice(k, k+step), numVal, k+step, j))
-	    jobs.append(p)
-	    p.start()
-	    p.join() #Wait until all the processes per row are finished before iterating the row
-
-    #Calculate Pivots
-    pivots = []
-    j = numClass - 1
-    col = numVal - 1
     
-    while j > 0:
-	ev = sharedErr.asarray()[j, col]
-	pivot_search = True
-	right = col
+    numErr = numVal
+    for j in xrange(1,numClass):
 	
-	while pivot_search:
-	    left_error = sharedErr.asarray()[j-1, right-1]
-	    right_error = sharedVar.asarray()[right, col]
-	    if left_error + right_error == ev:
-		pivots.insert(0, right)
-		col = right -1
-		pivot_search = False
-	    right -= 1
-	j -=1
+	partition = partitionErr(cores,numErr)
+	print partition
+    
+	#step= len(values)/cores #Naive split of the errorMat row into equal segments
+	#for k in range(0,len(values),step):
+	    #p = multiprocessing.Process(target=errPop, args=(sharedErr.asarray()[j], slice(k, k+step), numVal, k+step, j))
+	    #jobs.append(p)
+	    #p.start()
+	    #p.join() #Wait until all the processes per row are finished before iterating the row
+	    
+	numErr -= 1
 	
-    print pivots
+    exit()
+
+    ##Calculate Pivots
+    #pivots = []
+    #j = numClass - 1
+    #col = numVal - 1
+    
+    #while j > 0:
+	#ev = sharedErr.asarray()[j, col]
+	#pivot_search = True
+	#right = col
+	
+	#while pivot_search:
+	    #left_error = sharedErr.asarray()[j-1, right-1]
+	    #right_error = sharedVar.asarray()[right, col]
+	    #if left_error + right_error == ev:
+		#pivots.insert(0, right)
+		#col = right -1
+		#pivot_search = False
+	    #right -= 1
+	#j -=1
+	
+    #print pivots
     
 if __name__ == '__main__':
     multiprocessing.freeze_support()#For windows
@@ -145,3 +228,4 @@ if __name__ == '__main__':
     
     
     
+ 
